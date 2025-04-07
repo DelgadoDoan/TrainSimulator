822018be528180a8247fedd523311279a0dbb723
DelgadoDoan <doanmgd@gmail.com>
2025-04-07 23:36:07 +0800

    Fixed mobile support

--- Simulator/static/script.js
+++ Simulator/static/script.js
@@ -1,3 +1,13 @@
+const socket = new WebSocket('wss://trensimph.up.railway.app/ws/simulator');
+// 'wss://trensimph.up.railway.app/ws/simulator'
+// 'ws://127.0.0.1:8000/ws/simulator'
+
+let trains = {};
+
+const scale = 750 / 25;
+const offsetX = 1500 / 3;
+const offsetY = 750 / 20;
+
 lrt1Line = [
     {"x": 0, "y": 0},
     {"x": 0, "y": 1.87},
@@ -54,54 +64,99 @@ mrt3Line = [
 ]
 
 
-const c = document.getElementById("canvas");
-const ctx = c.getContext("2d");
+socket.onopen = () => {
+    console.log("Connected");
 
-c.width = window.innerWidth;
-c.height = window.innerHeight;
+    const c = document.getElementById("canvas");
+    const ctx = c.getContext("2d");
 
-let scale = c.width / 60;
-let offsetX = c.width / 3;
-let offsetY = c.height / 9;
+    c.width = 1500;
+    c.height = 750;
 
 
-drawRoutes = (line, color) => {
-    ctx.beginPath();
-    ctx.moveTo(line[0].x * scale + offsetX, line[0].y * scale + offsetY);
+    drawRoutes = (line, color) => {
+        ctx.beginPath();
+        ctx.moveTo(line[0].x * scale + offsetX, line[0].y * scale + offsetY);
 
-    for (let i = 1; i < line.length; i++) {
-        ctx.lineTo(line[i].x * scale + offsetX, line[i].y * scale + offsetY);
+        for (let i = 1; i < line.length; i++) {
+            ctx.lineTo(line[i].x * scale + offsetX, line[i].y * scale + offsetY);
+        }
+        
+        ctx.strokeStyle = color;
+        ctx.lineWidth = 2;
+
+        ctx.closePath();
+        ctx.stroke();
     }
-    
-    ctx.strokeStyle = color;
-    ctx.lineWidth = 2;
-
-    ctx.closePath();
-    ctx.stroke();
-}
 
 
-drawStations = (line, color) => {
-    for (let i = 0; i < line.length; i++) { 
-        let newBox = document.createElement("div");
-        newBox.classList.add("station");
-        newBox.id = `box-${i}`;
-    
-        newBox.style.left = `${line[i].x * scale + offsetX - 5}px`;
-        newBox.style.top = `${line[i].y * scale+ offsetY - 5}px`;
-        newBox.style.background = color
-    
-        let label = document.createElement("span");
-        newBox.appendChild(label);
-    
-        document.body.appendChild(newBox);
+    drawStations = (line, color) => {
+        for (let i = 0; i < line.length; i++) { 
+            let newBox = document.createElement("div");
+            newBox.classList.add("station");
+            newBox.id = `box-${i}`;
+        
+            newBox.style.left = `${line[i].x * scale + offsetX - 5}px`;
+            newBox.style.top = `${line[i].y * scale+ offsetY - 5}px`;
+            newBox.style.background = color
+        
+            let label = document.createElement("span");
+            newBox.appendChild(label);
+        
+            document.body.appendChild(newBox);
+        }
     }
-}
 
 
-drawRoutes(lrt1Line, "yellow");
-drawRoutes(lrt2Line, "purple");
-drawRoutes(mrt3Line, "blue");
-drawStations(lrt1Line, "yellow");
-drawStations(lrt2Line, "purple");
-drawStations(mrt3Line, "blue");
+    drawRoutes(lrt1Line, "yellow");
+    drawRoutes(lrt2Line, "purple");
+    drawRoutes(mrt3Line, "blue");
+    drawStations(lrt1Line, "yellow");
+    drawStations(lrt2Line, "purple");
+    drawStations(mrt3Line, "blue");
+};
+
+socket.onmessage = (event) => {
+    let data = JSON.parse(event.data);
+
+    for (let train of data.message) {
+        let trainId = train.id;
+        let trainLine = train.line
+
+        // If the box for this train does not exist, create it
+        if (!trains[trainId]) {
+            let trainContainer = document.createElement("div");
+            trainContainer.classList.add("train-container");
+            trainContainer.id = `${trainId}`; // Unique ID for each train
+
+            let trainBox = document.createElement("div");
+            trainBox.classList.add("train");
+            trainContainer.appendChild(trainBox);
+            trainBox.style.background = train.status === "running" ? "green" : "red";
+
+            let label = document.createElement("div");
+            label.classList.add("train-label");
+
+            label.innerText = `Line: ${trainLine}\nID: ${trainId}\n`;
+            trainContainer.appendChild(label)
+
+            document.body.appendChild(trainContainer);
+            trains[trainId] = trainContainer;
+        }
+
+        let newX = train.pos.x * scale + offsetX - 5; // Adjust scaling
+        let newY = train.pos.y * scale + offsetY - 5;
+
+        // Update train position
+        trains[trainId].style.left = newX + "px";
+        trains[trainId].style.top = newY + "px";
+
+        let trainBox = trains[trainId].querySelector(".train");
+        trainBox.style.background = train.status === "running" ? "green" : "red";    
+    }
+};
+
+
+socket.onclose = () => {
+    console.log("Disconnected");
+};
